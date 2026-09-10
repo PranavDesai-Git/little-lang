298cd5a9007d3b5d1bef52b8427904ae7cdc1893
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-10 15:58:11 +0530

    feat: add envInsert and envGet

--- src/HashTable.c
+++ src/HashTable.c
@@ -1,5 +1,6 @@
 #include "TreeNode.h"
 #include <stdint.h>
+#include <string.h>
 #define SIZE 1024
 
 typedef Node *(*Func)(Node *left, Node *right);
@@ -9,10 +10,14 @@ typedef struct EnvEntry {
     uint_fast8_t isFunc; // 1 if is func 0 if var
 
     union {
-        int node;
+        Node *node;
         Func func;
     } val;
+
+    struct EnvEntry *next; // incase of collisions
 } EnvEntry;
+
+EnvEntry *envTable[SIZE];
 
 unsigned long hashString(char *str) {
     unsigned long hash = 5381;
@@ -20,4 +25,37 @@ unsigned long hashString(char *str) {
     while ((c = *str++))
         hash = ((hash << 5) + hash) + c;
     return hash % SIZE;
+}
+
+void envInsert(EnvEntry *newEntry) {
+    int pos = hashString(newEntry->key);
+    if (envTable[pos] != NULL) {
+        if (strcmp(envTable[pos]->key, newEntry->key)) {
+            envTable[pos]->val = newEntry->val;
+            return;
+        }
+        EnvEntry *temp = envTable[pos];
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newEntry;
+    } else {
+        envTable[pos] = newEntry;
+    }
+}
+
+EnvEntry *getEnvEntry(char *key) {
+    int pos = hashString(key);
+    if (envTable[pos] == NULL)
+        return NULL;
+    if (strcmp(envTable[pos]->key, key)) {
+        EnvEntry *temp = envTable[pos];
+        while (temp != NULL) {
+            temp = temp->next;
+            if (strcmp(temp->key, key))
+                break;
+        }
+        return temp;
+    }
+    return envTable[pos];
 }
